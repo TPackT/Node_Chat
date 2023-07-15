c888643a29874851c291ad7177fae7f06c173e81
nguq03 <nguq03@vse.cz>
2023-07-15 21:00:37 +0200

    view messages from db

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+/*
+
 export default {
   client: "sqlite3",
   connection: {
@@ -5,28 +7,24 @@ export default {
   },
   useNullAsDefault: false,
 }
-
-
-
-/*
+*/
 //dev/test version
 
 export default {
-      development: {
-        client: "sqlite3",
-        connection: {
-          filename: "./mydb.sqlite",
-        },
-        useNullAsDefault: true,
-        debug: false,
-      },
-      test: {
-        client: "sqlite3",
-        connection: {
-          filename: ":memory:",
-        },
-        useNullAsDefault: false,
-        debug: false,
-      },
-    }
-    */
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./mydb.sqlite",
+    },
+    useNullAsDefault: true,
+    debug: false,
+  },
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    useNullAsDefault: false,
+    debug: false,
+  },
+}
--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express"
+import { db } from "./database.js"
 
 export const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", async (req, res) => {
-    res.render("index")
+    const messages = await db("messages").select("*")
+    
+    res.render("index", {
+        messages: messages,
+    })
 })
