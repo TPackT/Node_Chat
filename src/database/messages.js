c1c3388bd203ca06882c30a1e45f09e49d8a1a96
nguq03 <nguq03@vse.cz>
2023-07-18 20:34:04 +0200

    added user db

--- src/database/messages.js
+++ src/database/messages.js
@@ -2,7 +2,6 @@ import { db }from "../database.js"
 
 export const getAllMessagesByChatroom = async (chatroomId) => {
     const messages = await db("messages").select("*").where("chatroomId", chatroomId)
-    console.log("messages by chatroomId" + messages)
+
     return messages
 }
-
