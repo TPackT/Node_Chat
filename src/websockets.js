9057be7b8c8bc75f6c99b64e98005ea15f48bfd3
nguq03 <nguq03@vse.cz>
2023-07-24 19:58:23 +0200

    chatroom specific websockets

--- src/websockets.js
+++ src/websockets.js
@@ -3,22 +3,32 @@ import ejs from "ejs"
 import { getAllMessagesByChatroom, getAllMessagesWithUsernames } from "./database/messages.js"
 import { getUserByToken } from "./database/users.js"
 
-/** @type {Set<Websocket>} */
-const connections = new Set()
+/** @type {Map<string, Set<Websocket>>} */
+const connectionsByChatroom = new Map()
 
 export const createWebSocketServer = (server) => {
   const wss = new WebSocketServer({ server })
 
   wss.on("connection", async (ws, req) => {
-    connections.add(ws)
-    console.log("New connection", connections.size)
+    
     const token = req.headers.cookie.slice(6)
     try {
       const user = await getUserByToken(token)
       if (user) {
         ws.user = user
+        const chatroomId = req.url.substring(1)
+
+        if(!connectionsByChatroom.has(chatroomId)) {
+          connectionsByChatroom.set(chatroomId, new Set())
+        }
+
+        connectionsByChatroom.get(chatroomId).add(ws)
+        console.log(`User ${user.id} connected to chatroom ${chatroomId}`)
+        console.log("New connection", connectionsByChatroom.size)
+
       } else {
         console.error("User not found")
+        ws.close()
       }
     } catch (e) {
       console.error("Error retrieving user information")
@@ -26,8 +36,12 @@ export const createWebSocketServer = (server) => {
 
 
     ws.on("close", () => {
-      connections.delete(ws)
-      console.log("Closed connection", connections.size)
+      const { chatroomId } = ws
+      if (chatroomId && connectionsByChatroom.has(chatroomId)) {
+        // Remove the WebSocket connection from the chatroom's set
+        connectionsByChatroom.get(chatroomId).delete(ws);
+        console.log(`User ${ws.user.id} disconnected from chatroom ${chatroomId}`)
+      }
     })
   })
 }
@@ -36,7 +50,10 @@ export const sendMessagesToAllConnections = async (chatroomId) => {
   const messages = await getAllMessagesByChatroom(chatroomId)
   const messagesWithUsernames = await getAllMessagesWithUsernames(messages)
 
-  for (const connection of connections) {
+
+  const chatroomConnections = connectionsByChatroom.get(chatroomId)
+  if  (chatroomConnections) {
+  for (const connection of chatroomConnections) {
     const {user} = connection
     if (user) {
       const userId = user.id
@@ -51,9 +68,12 @@ export const sendMessagesToAllConnections = async (chatroomId) => {
         html: html
       }
       
-      console.log("Sent to all")
       const json = JSON.stringify(message)
       connection.send(json)
+      console.log(`Sent message updates to user ${userId} in chatroom ${chatroomId}`)
+      }
     }
+  } else {
+    console.log(`No WebSocket connections found for chatroom ${chatroomId}`)
   }
 }
